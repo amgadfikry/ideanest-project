cb57243a0bac24b2f16a5b826152c87fd07ee56f
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-10-28 00:23:20 +0300

    Use filter error globally and transform data interceptor

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,8 @@ import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import { swaggerConfig } from './config/swagger.config';
 import * as cookieParser from 'cookie-parser';
+import { GlobalExceptionsFilter } from './common/filters/global-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 // This function is the entry point of the application.
 async function bootstrap() {
@@ -18,6 +20,15 @@ async function bootstrap() {
   // Create the Swagger document and set up the Swagger UI
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
+
+  // Apply the global validation pipe that validates payloads against the DTO classes
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true, // Automatically transform payloads to DTO instances
+    whitelist: true, // Strip properties that are not in the DTO
+  }));
+
+  // Apply the global exception filter that formats exceptions as JSON responses
+  app.useGlobalFilters(new GlobalExceptionsFilter());
 
   // Enable cookie parsing
   app.use(cookieParser());
