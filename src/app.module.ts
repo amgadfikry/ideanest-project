87993be25bda8fbbbb059b5ed260baef27fa1918
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-10-27 23:21:36 +0300

    Create main module for application
    - Import application modules
    - Connect to Redis and MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,25 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { mongoDBConfig } from './config/mongodb.config';
+import { redisConfig } from './config/redis.config';
+import { UserModule } from './user/user.module';
+import { PasswordModule } from './password/password.module';
+import { AuthModule } from './auth/auth.module';
+import { TransactionModule } from './transaction/transaction.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import { OrganizationModule } from './organization/organization.module';
 
+// Main module of the application to import all the other modules
 @Module({
-  imports: [],
+  imports: [
+    CacheModule.registerAsync(redisConfig), // Register the Redis cache
+    MongooseModule.forRoot(mongoDBConfig.uri), // Connect to MongoDB
+    UserModule,
+    PasswordModule,
+    AuthModule,
+    TransactionModule,
+    OrganizationModule,
+  ],
   controllers: [],
   providers: [],
 })
